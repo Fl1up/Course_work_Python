32d55e9233cfcea1f09ec271d5df5bc14d0b68eb
German <germanstarinenko@gmail.com>
2023-03-17 15:31:21 +0300

    example warning

--- Utils.py
+++ Utils.py
@@ -1,5 +1,7 @@
 import json
 import random
+from datetime import datetime
+
 
 def load_json():
     with open("operations.json","r") as file:
@@ -7,3 +9,16 @@ def load_json():
         random.shuffle(operations_json)
         for i in operations_json:
             return i
+
+
+def load_dates(load_dict):
+    load_dates = (load_dict["date"].split("T")[0])
+    return load_dates
+
+
+def number_card(number: str):
+    number_card = number["from"].split()[len(number["from"].split()) - 1]
+    card = f'{number["from"].replace(f" {number_card}", "")}'
+    number_card = number_card[:-10] + "** **** " + number_card[12:]
+    number_card = f"{card} {number_card[:4]} {number_card[4:]}"
+    return number_card
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from Utils import load_json
+from Utils import load_dates
+from Utils import number_card
+
+
 def main():
     for count_operations in range(5):
         load_dict = load_json()
         for key, values in load_dict.items():
             if values == "EXECUTED":
                 load_to = load_dict["to"]
-                load_dates = (load_dict["date"].split("T")[0])
 
-                number_card = f'{load_dict["from"].split()[len(load_dict["from"].split()) - 1]}'
-                number_card = number_card[:-10] + '** **** ' + number_card[12:]
-                number_card = f'{number_card[:4]} {number_card[4:]}'
-
-                print(f'{load_dates} {load_dict["description"]}\n'
-                      f'{number_card} -> Счет {"**" + load_to[20:]}\n'
+                print(f'{load_dates(load_dict)} {load_dict["description"]}\n'
+                      f'{number_card(load_dict)} -> Счет {"**" + load_to[20:]}\n'
                       f'{load_dict["operationAmount"]["amount"]} {load_dict["operationAmount"]["currency"]["name"]}\n')
 
+
 main()
+""" Сделать формат даты .. убрать ошибку по фор """
