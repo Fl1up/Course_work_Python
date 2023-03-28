7fe21b7145260c9326bd9191bf8bb06e11537399
German <germanstarinenko@gmail.com>
2023-03-28 08:15:47 +0300

    1.2

--- tests/test_unittest.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from Utils import load_json
-
-class TestCalc(unittest.TestCase):
-    def test_unittest(self):
-        self.assertRaises(FileNotFoundError, load_json("operations.json"), load_json('random'))
