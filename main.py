f8060652f7c4d51683a3875281aacbeb058b6408
German <germanstarinenko@gmail.com>
2023-03-29 12:54:22 +0300

    1.6

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from Utils import load_json, number_card, load_dates
 
-
 def main():
     operations = []
     for _ in range(5):
@@ -11,11 +10,11 @@ def main():
 
         for keys, values in load_dict.items():
             if values == "EXECUTED":
-
                 load_to = load_dict["to"]
+
                 print(f'{load_dates(operation)} {load_dict["description"]}\n'
-                f'{number_card(load_dict)} -> Счет {"**" + load_to[20:]}\n'
-                f'{load_dict["operationAmount"]["amount"]} {load_dict["operationAmount"]["currency"]["name"]}\n')
+                    f'{number_card(load_dict)} -> Счет {"**" + load_to[20:]}\n'
+                    f'{load_dict["operationAmount"]["amount"]} {load_dict["operationAmount"]["currency"]["name"]}\n')
 
 
 main()
